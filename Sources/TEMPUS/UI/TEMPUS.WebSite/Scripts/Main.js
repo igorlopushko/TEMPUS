2c72a28622f48ddffb7ebf9970d8df98017253f8
Alexander Zayats <sanyazayats@gmail.com>
2014-04-03 00:19:37 +0300

    Create Projects page and Create page for project

--- Sources/TEMPUS/UI/TEMPUS.WebSite/Scripts/Main.js
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Scripts/Main.js
@@ -1,1 +1,7 @@
-﻿
+﻿// Create Project Carusel
+$(document).ready(function (ev) {
+    $('#create-carusel').on('slide.bs.carousel', function (evt) {
+        $('#create-carusel .controls li.active').removeClass('active');
+        $('#create-carusel .controls li:eq(' + $(evt.relatedTarget).index() + ')').addClass('active');
+    })
+});
