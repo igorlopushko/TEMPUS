9ce5af1c80f30a865ab7ec0c8cb007910cc3b172
zWeRz <wer3452@gmail.com>
2014-04-09 22:44:11 +0300

    Team page added

---
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Controllers/TeamController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEMPUS.WebSite.Controllers
+{
+    public class TeamController : Controller
+    {
+        //
+        // GET: /Team/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
