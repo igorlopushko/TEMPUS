415e27f76aab589dd1102a8ea67e6c44b5fc955b
igorlopushko <lopushko.igor@gmail.com>
2014-04-13 15:26:17 +0300

    Projects added to source control

---
+++ Sources/TEMPUS/Databases/TEMPUS.DB.Migration/UserTableV1.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace TEMPUS.DB.Migration
+{
+    /// <summary>
+    /// Keep migration date in following format: YYYYMMDD
+    /// </summary>
+    [Migration(20140412)]
+    public class UserTableV1 : FluentMigrator.Migration
+    {
+        public override void Down()
+        {
+            Execute.EmbeddedScript("User.drop.v1.sql");
+            Execute.EmbeddedScript("sp_GetUserById.drop.v1.sql");
+        }
+
+        public override void Up()
+        {
+            Execute.EmbeddedScript("User.create.v1.sql");
+            Execute.EmbeddedScript("sp_GetUserById.create.v1.sql");
+        }
+    }
+}
