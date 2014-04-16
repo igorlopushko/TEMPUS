14fd87e76e6a6008c7ba9681e6af12595721b518
satir- <ileshiyi@gmail.com>
2014-04-16 23:03:12 +0200

    Tasks view added

---
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Controllers/RisksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEMPUS.WebSite.Controllers
+{
+    public class RisksController : Controller
+    {
+        //
+        // GET: /Risks/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEMPUS.WebSite.Controllers
+{
+    public class TasksController : Controller
+    {
+        //
+        // GET: /Tasks/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
