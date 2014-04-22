e95a53da1e4e7da717a7da6a90d8d376aacca250
Yaroslav Admin <devoto13@gmail.com>
2014-04-23 01:17:34 +0300

    Added current menu item highlighting & removed redundant Home link

---
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Helpers/PageHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace TEMPUS.WebSite.Helpers
+{
+    public static class PageHelper
+    {
+        public static bool IsCurrentUrl(this UrlHelper helper, string actionName, string controllerName)
+        {
+            return helper.RequestContext.RouteData.Values["controller"].ToString() == controllerName &&
+                helper.RequestContext.RouteData.Values["action"].ToString() == actionName;
+        }
+
+        public static HtmlString MainMenuItem(this HtmlHelper helper, string linkText, string actionName, string controllerName)
+        {
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+
+            var tagBuilder = new TagBuilder("li");
+            if (urlHelper.IsCurrentUrl(actionName, controllerName))
+            {
+                tagBuilder.AddCssClass("active");
+            }
+            tagBuilder.InnerHtml = helper.ActionLink(linkText, actionName, controllerName).ToHtmlString();
+
+            return new HtmlString(tagBuilder.ToString());
+        }
+    }
+}
--- Sources/TEMPUS/UI/TEMPUS.WebSite/Scripts/Main.js
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Scripts/Main.js
@@ -1,20 +1,7 @@
 ﻿// Create Project Carusel
 $(document).ready(function (ev) {
-    $('#create-carusel').on('slide.bs.carousel', function (evt) {
+    $('#create-carusel').on('slide.bs.carousel', function(evt) {
         $('#create-carusel .controls li.active').removeClass('active');
         $('#create-carusel .controls li:eq(' + $(evt.relatedTarget).index() + ')').addClass('active');
-    })
-});
-
-// test function for highlighting menu section of current page
-$(document).ready(function (ev) {
-    var urlPieces = document.URL.split('http://').join('').split('/');
-    if (urlPieces[1] == '') {
-        $("#menu-list li").first().addClass("active");
-    } else {
-        $("#menu-list li")
-          .filter(function (index) {
-              return $(this).text().trim() == urlPieces[1];
-          }).addClass("active");
-    }
+    });
 });
