67895afc9980c3153f039bd3a39d906da3a375f4
Yaroslav Admin <devoto13@gmail.com>
2014-04-23 03:10:59 +0300

    Added ViewModel for Time Record

---
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Models/Project/TimeRecordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TEMPUS.WebSite.Models.Team;
+
+namespace TEMPUS.WebSite.Models.Project
+{
+    public class TimeRecordViewModel
+    {
+        public TaskViewModel Task { get; set; }
+        public ProjectViewModel Project { get; set; }
+        public UserViewModel User { get; set; }
+        public double Hours { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+    }
+}
