bdd4e2e27ad7ddafe6d9a8acbf192755d1fc407c
Yaroslav Admin <devoto13@gmail.com>
2014-04-27 19:03:57 +0300

    Removed DB.Migration Project

--- Sources/TEMPUS/Databases/TEMPUS.DB.Migration/Rollback.bat
+++
@@ -1,1 +0,0 @@
-..\..\Lib\Migrator\Migrate.exe /connection "Data Source=.;Integrated Security=SSPI;Initial Catalog=TEMPUS;Persist Security Info=True" /db sqlserver /target bin\Debug\TEMPUS.DB.Migration.dll /task rollback
