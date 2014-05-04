2b2ae6439e59fa4f97c932c2d63b256f1bd522cc
igorlopushko <lopushko.igor@gmail.com>
2014-05-05 01:23:01 +0300

    Department file added

--- Sources/TEMPUS/Databases/DB/Models/Project/Department.cs
+++ Sources/TEMPUS/Databases/DB/Models/Project/Department.cs
@@ -2,11 +2,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TEMPUS.DB.Models.User
+namespace TEMPUS.DB.Models.Project
 {
-    public class Permission : Entity
+    public class Department : Entity
     {
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
         [MaxLength(64)]
