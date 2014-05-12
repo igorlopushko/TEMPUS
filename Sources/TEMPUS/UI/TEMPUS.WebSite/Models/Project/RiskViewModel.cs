be0a2c474b75ccf0f1bee28a83e821118193938d
Alexander Zayats <sanyazayats@gmail.com>
2014-05-12 22:52:58 +0300

    Added RiskViewModel for CreateProjectPage, added TaskListViewModel for task list page

---
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Models/Project/RiskViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TEMPUS.WebSite.Models.Project;
+
+namespace TEMPUS.WebSite.Models.Task
+{
+    public class RiskViewModel
+    {
+        public String Description { get; set; }
+        public String Type { get; set; }
+        public String Probability { get; set; }
+        public String Impact { get; set; }
+        public String Response { get; set; }
+        public String Status { get; set; }
+        public String Person { get; set; }
+        public DateTime FinishDate { get; set; }
+    }
+}
---
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Models/Task/TaskListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TEMPUS.WebSite.Models.Project;
+
+namespace TEMPUS.WebSite.Models.Task
+{
+    public class TaskListViewModel
+    {
+        public String Name { get; set; }
+        public String Person { get; set; }
+        public TaskStatus Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
