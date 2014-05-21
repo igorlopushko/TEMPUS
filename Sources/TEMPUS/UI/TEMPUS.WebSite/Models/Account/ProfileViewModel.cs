b2efa3189c562fd2e35da82846e8e685a62b1ad8
zWeRz <wer3452@gmail.com>
2014-05-22 02:11:04 +0300

    Edit profile validation added

--- Sources/TEMPUS/UI/TEMPUS.WebSite/Models/Account/ProfileViewModel.cs
+++ Sources/TEMPUS/UI/TEMPUS.WebSite/Models/Account/ProfileViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using TEMPUS.BaseDomain.Messages.Identities;
 
 namespace TEMPUS.WebSite.Models.Account
@@ -20,16 +21,23 @@ namespace TEMPUS.WebSite.Models.Account
         /// <summary>
         /// Gets or sets the user first name.
         /// </summary>
+        [Required]
+        [MaxLength(50, ErrorMessage = "Length of the FirstName needs to be less than 50 symbols.")]
+        [MinLength(2, ErrorMessage = "Length of the FirstName needs to be more than 1 symbol.")]
         public string FirstName { get; set; }
 
         /// <summary>
         /// Gets or sets the user last name.
         /// </summary>
+        [Required]
+        [MaxLength(50, ErrorMessage = "Length of the LastName needs to be less than 50 symbols.")]
+        [MinLength(2, ErrorMessage = "Length of the LastName needs to be more than 1 symbol.")]
         public string LastName { get; set; }
 
         /// <summary>
         /// Gets or sets the phone of the user.
         /// </summary>
+        [RegularExpression(@"\+[0-9]{10,15}", ErrorMessage = "Phone number should be like +46396234567")]
         public string Phone { get; set; }
 
         /// <summary>
@@ -40,6 +48,7 @@ namespace TEMPUS.WebSite.Models.Account
         /// <summary>
         /// Gets or sets the date of birth of the user.
         /// </summary>
+        [Required]
         public DateTime DateOfBirth { get; set; }
 
         /// <summary>
